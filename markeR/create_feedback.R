5bb974faec8061ca4e9349ff833b89ecd09d07ea
Jonathan Marshall <jmarshall@never.you.mind>
2019-09-16 10:28:28 +1200

    tidy ups from 227.212, sem1 2019

--- markeR/create_feedback.R
+++ markeR/create_feedback.R
@@ -1,6 +1,7 @@
 # load in the database reading stuff
 source("marks_csv.R")
 source("questions_csv.R")
+library(fs)
 
 collapse <- function(comments) {
   comments <- unlist(lapply(comments, function(x) { paste(x, collapse="\n") }))
@@ -13,24 +14,42 @@ create_mark <- function(Mark, Total, Award) {
   ifelse(!is.na(Award) & Award == "star", paste(mark_fraction, "\\includegraphics[width=0.5cm]{star.png}"), mark_fraction)
 }
 
-create_feedback <- function(marks) {
-  student = marks %>% select(StudentID, StudentName) %>% unique() %>% as.list()
-  questions = marks %>% select(Question, Mark, Total, Award, Comments) %>% mutate(Comments = collapse(Comments))
+create_feedback <- function(marks, out_dir, clean=TRUE) {
+  student = marks %>% select(StudentID, StudentName, PDFurl) %>% unique() %>% as.list()
+  questions = marks %>% select(Exercise, Question, Mark, Total, Award, Comments) %>% mutate(Comments = collapse(Comments)) %>%
+    split(.$Exercise)
   # add total row
-  tab_data <- bind_rows(questions, tibble(Question = "Total", Mark = sum(questions$Mark), Total = sum(questions$Total), Comments = "")) %>%
-    mutate(Mark = create_mark(Mark, Total, Award)) %>% select(Question, Mark, Comments)
+  tab_data <- lapply(questions, 
+                     function(x) {
+                       bind_rows(x, tibble(Question = "Total", Mark = sum(x$Mark), Total = sum(x$Total), Comments = "")) %>%
+                         mutate(Mark = create_mark(Mark, Total, Award)) %>% select(Question, Mark, Comments)
+                     })
+  total = marks %>% select(Mark, Total) %>% summarise(Mark = sum(Mark), Total = sum(Total)) %>% as.list()
   # create environment
   student.envir = new.env()
   student.envir$name = student$StudentName
   student.envir$id   = student$StudentID
+  student.envir$overall = total
   student.envir$marks = tab_data
   # render markdown
-  rmarkdown::render("feedback.Rmd", output_format='pdf_document', output_file = paste0(student$StudentName, ".pdf"),
-                    output_dir = "../test_assignment/feedback", envir=student.envir)
+  student_outdir <- file.path(out_dir, path_dir(student$PDFurl))
+  rmarkdown::render("feedback.Rmd", output_format='pdf_document', output_file = "feedback.pdf",
+                    output_dir = student_outdir, envir=student.envir, clean=clean)
 }
 
 feedback <- function() {
   # read in the marks
-  marks = read_marks() %>% left_join(read_questions() %>% select(-Comments, Total=Marks))
-  marks %>% split(marks$StudentID) %>% lapply(create_feedback)
+  marks = read_marks() %>% left_join(read_questions() %>% select(-Comments, Total=Marks)) %>%
+    tidyr::extract(Question, into=c("Exercise", "Question"), regex="([0-9]+).([0-9]+)", remove=TRUE, convert=TRUE) %>%
+    mutate(Question = as.character(Question)) %>%
+    # HACK
+    mutate(Mark = as.numeric(Mark))
+  # HACK
+  marks %>% split(.$StudentID) %>% lapply(create_feedback, out_dir = "../227212_ass1/feedback")
 }
+
+# fixup a comment for Olivia
+marks$Comments <- lapply(marks$Comments, function(x) { sub("simmetry", "symmetry", x) })
+
+# dump marks out to spreadsheet
+read_marks() %>% group_by(StudentID, StudentName) %>% summarise(Mark = sum(Mark)) %>% write_csv("../227212_ass1/227212_ass1.csv")
